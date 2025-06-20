53ca16f8f96844a435115ed3efafd649d20ca5d0
Akshintala Sri Harsha <akshintalasriharsha@yahoo.com>
2025-06-20 15:10:49 +0530

    fundamentals

---
+++ Assignments/JavaScript/variables.js
@@ -0,0 +1,24 @@
+let name = "Sri Harsha";
+let email = "sample@samplemail.com";
+let mobile = "1234567891";
+let password = "password";
+let address = "Andhra Pradesh";
+let pincode = "123456";
+let monthlySalary = 50000;
+let annualSalary = 1200000;
+console.log(name);
+console.log(email);
+console.log(mobile);
+console.log(password);
+console.log(address);
+console.log(pincode);
+console.log(monthlySalary);
+console.log(annualSalary);
+
+let productId = 12;
+let productTitle = "PC";
+let productPrice = 100000;
+console.log(productId, productTitle, productPrice);
+
+productPrice = 120000;
+console.log(productPrice);
---
+++ Javascript/firstCode.js
@@ -0,0 +1,18 @@
+console.log("Coding is easy and fun");
+
+console.log("Practice everyday");
+
+console.log(12345);
+
+let userName = "Harsha";
+
+let userEmail = "sample@samplemail.com";
+
+let userNumber = "1234567891";
+
+console.log(userName);
+console.log(userEmail);
+console.log(userNumber);
+
+userName = "Sri Harsha";
+console.log(userName);
