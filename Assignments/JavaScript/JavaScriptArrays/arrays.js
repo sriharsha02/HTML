fce6867b4ba2296b3841ea33057e8f000ad4a08d
Akshintala Sri Harsha <akshintalasriharsha@yahoo.com>
2025-06-21 11:10:05 +0530

    arrays basics

---
+++ Assignments/JavaScript/JavaScriptArrays/arrays.js
@@ -0,0 +1,35 @@
+let names = ["sri", "harsha", "akshintala", "sriharsha", "ashi"];
+
+let emails = [
+  "sri@sample.com",
+  "harsha@sample.com",
+  "akshintala@sample.com",
+  "sriharsha@sample.com",
+  "ashi@sample.com",
+];
+
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
+console.log(names[4]);
+
+console.log(emails[0]);
+console.log(emails[1]);
+console.log(emails[2]);
+console.log(emails[3]);
+console.log(emails[4]);
+
+let videoTitles = [
+  "How to Create a GitHub Profile That Gets You a Software Job ",
+  "How to Buy a Domain Name",
+  "How Amazon Works: Part 5",
+  "How Amazon Works: Part 4 ",
+  "How Amazon Works: Part 3",
+];
+
+console.log(videoTitles);
+videoTitles[3] = "How Amazon Works: Part 6 ";
+videoTitles[4] = "How Amazon Works: Part 7";
+
+console.log(videoTitles);
---
+++ Javascript/arrays.js
@@ -0,0 +1,56 @@
+let topic1 = "1. Introduction";
+let topic2 = "2. HTML Page syntax";
+let topic3 = "3. Create HTML page and check output";
+let topic4 = "4. HTML, head, body tags";
+
+let topics = [
+  "1. Introduction",
+  "2. HTML Page syntax",
+  "3. Create HTML page and check output",
+  "4. HTML, head, body tags",
+];
+
+console.log(topics);
+
+let courses = [
+  "React JS",
+  "Java Springboot",
+  "Automation Testing",
+  "Data Analytics",
+  "Data Science",
+  "Android app development",
+];
+
+console.log(courses[0]);
+console.log(courses[1]);
+console.log(courses[2]);
+console.log(courses[3]);
+console.log(courses[4]);
+console.log(courses[5]);
+
+console.log(topics[0]);
+console.log(topics[1]);
+console.log(topics[2]);
+console.log(topics[3]);
+
+let syllabus = ["intro", "html"];
+console.log(syllabus.length);
+
+syllabus.push("bootstrap");
+console.log(syllabus);
+
+console.log(syllabus.length);
+console.log(syllabus[0]);
+syllabus[0] = "introduction";
+
+console.log(syllabus[0]);
+
+syllabus[2] = "CSS/Styles";
+
+console.log(syllabus);
+
+let prices = [25, 30, 15, 12, "ReactJS"];
+
+console.log(prices[1]);
+console.log(prices[3]);
+console.log(prices[4]);
