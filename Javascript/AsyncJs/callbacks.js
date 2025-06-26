21a06a37a797b0f303a9d1db021628a3b8662e2a
Akshintala Sri Harsha <akshintalasriharsha@yahoo.com>
2025-06-26 09:23:42 +0530

    asyn await

---
+++ Javascript/AsyncJs/callbacks.js
@@ -0,0 +1,18 @@
+let firstName = "Sri Harsha";
+let lastName = "Akshintala";
+
+function concat(str1, str2) {
+  console.log(str1 + " " + str2);
+}
+
+concat(firstName, lastName);
+
+function add(num1, num2) {
+  console.log(num1 + num2);
+}
+
+let firstNumber = 10;
+let secondNumber = 20;
+add(firstNumber, secondNumber);
+
+add(2, 3);
